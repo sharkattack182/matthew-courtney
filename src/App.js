da09dc71c1349839e3cd8133c79c824fd81d5cea
Matthew Courtney <matthewdc182@gmail.com>
2020-10-22 02:24:49 -0400

    got the sidebar to act responsivley

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import './App.css';
 import Sidebar from './components/Sidebar/sidebar'
 import Body from './components/Main/body'
+import Row from 'react-bootstrap/Row'
 
 function App() {
   return (
     <div className="App">
-  <Sidebar />
+    <Row >
+      <div className="col-xs-12"><Sidebar /></div>
+    </Row>
+  
   <Body />
     </div>
   );
