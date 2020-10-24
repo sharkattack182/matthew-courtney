e87796713e14a89d55e556d9eee49841d81eec4b
Matthew Courtney <matthewdc182@gmail.com>
2020-10-23 22:23:24 -0400

    added in routes

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import './App.css';
 import Sidebar from './components/Sidebar/sidebar'
 import Welcome from './components/Welcome/welcome'
 import Portfolio from './components/Portfolio/portfolio'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 
 function App() {
   return (
     <div className="App">
- <Sidebar />
-  
-  <Portfolio />
+      <Sidebar />
+      <Route exact path="/" component={Welcome} />
+      <Route exact path="/portfolio" component={Portfolio} />
     </div>
   );
 }
