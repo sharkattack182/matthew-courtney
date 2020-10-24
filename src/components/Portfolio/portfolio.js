6060229fb9a591d90ee8f1972cbf17d7bba1aac4
Matthew Courtney <matthewdc182@gmail.com>
2020-10-23 22:13:50 -0400

    added the cards content

--- src/components/Portfolio/portfolio.js
+++ src/components/Portfolio/portfolio.js
@@ -8,15 +8,20 @@ function Portfolio() {
     return (
         <div className="container-custom">
             <div className="portfolio">
-                <h1>Portfolio</h1>
+                <h1 className="header">Portfolio</h1>
                 <div className="row">
                     <div className="col-lg-3">
                         <div className="card">
                             <div className="image image1"></div>
                             <div className="content">
-                                <div className="header">Cool Title</div>
-                                <div className="meta">By Matt</div>
-                                <div className="description">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis maiores expedita exercitationem rerum, voluptate laudantium nostrum ratione aliquam adipisci, vitae soluta repellat cupiditate, inventore ipsa distinctio quidem assumenda voluptates eligendi.</div>
+                                <div className="header">EquiPPE</div>
+                                <div className="meta">Matthew Courtney</div>
+                                <div className="description">This was the final project for my bootcamp. Our team decided to create a functioning shopping cart for an ecommerce site. I was responsible for building and maintaining the repository as well as delegating tasks and responsibilities.
+                                I also created and integrated teh database using sequelize and defined all of the server side routes using express. Finally I styled the UI using HTML CSS and Javascript.</div>
+                                <a href="https://vast-ravine-19894.herokuapp.com/" target="_blank"><button type="button"
+                                    class="btn btn-info zoom"><i class="fas fa-link"></i></button></a>
+                                <a href="https://github.com/sharkattack182/project-3" target="_blank"><button type="button"
+                                    class="btn btn-info zoom"><i class="fab fa-github"></i></button></a>
                             </div>
                         </div>
                     </div>
@@ -25,19 +30,29 @@ function Portfolio() {
                         <div className="card">
                             <div className="image image2"></div>
                             <div className="content">
-                                <div className="header">Cool Title</div>
-                                <div className="meta">By Matt</div>
-                                <div className="description">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis maiores expedita exercitationem rerum, voluptate laudantium nostrum ratione aliquam adipisci, vitae soluta repellat cupiditate, inventore ipsa distinctio quidem assumenda voluptates eligendi.</div>
+                                <div className="header">Classmate Tracker</div>
+                                <div className="meta">Matthew Courtney</div>
+                                <div className="description">The classmate tracker is an app developed using react.js. The idea was to dynamically create a table of classmates using a JSON object. I then wrote the logic to add the ability to search and filter all of the results. I was very
+                                pleased with the functionability as well as the way the design came out.</div>
+                                <a href="https://powerful-lowlands-68943.herokuapp.com/" target="_blank"><button type="button"
+                                    class="btn btn-info zoom"><i class="fas fa-link"></i></button></a>
+                                <a href="https://github.com/sharkattack182/react-employee" target="_blank"><button type="button"
+                                    class="btn btn-info zoom"><i class="fab fa-github"></i></button></a>
                             </div>
                         </div>
                     </div>
-                    <div className="col-lg-3">
+                    <div className="col-lg-3 card3">
                         <div className="card">
                             <div className="image image3"></div>
                             <div className="content">
-                                <div className="header">Cool Title</div>
+                                <div className="header">Pokedex</div>
                                 <div className="meta">By Matt</div>
-                                <div className="description">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis maiores expedita exercitationem rerum, voluptate laudantium nostrum ratione aliquam adipisci, vitae soluta repellat cupiditate, inventore ipsa distinctio quidem assumenda voluptates eligendi.</div>
+                                <div className="description">The pokedex project was an excersize in express. The app is styled very basically with bootstrap, CSS and HTML. For this project we used a web based API called Pokemon API. Once we found this we used an axios call to render the data we then built our own API routes to allow a user to add pokemon to their own pokedex. We just used jquery to then parce out all of
+                                the information we wanted to display.</div>
+                                <a href="https://vast-escarpment-44555.herokuapp.com/" target="_blank"><button type="button"
+                                    class="btn btn-info zoom"><i class="fas fa-link"></i></button></a>
+                                <a href="https://github.com/sharkattack182/react-employee" target="_blank"><button type="button"
+                                    class="btn btn-info zoom"><i class="fab fa-github"></i></button></a>
                             </div>
                         </div>
                     </div>
