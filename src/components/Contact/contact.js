9b4849a0af07a122de2a8892b1742714a8f32e19
Matthew Courtney <matthewdc182@gmail.com>
2020-11-04 21:54:47 -0500

    added icons on contact page :

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -9,9 +9,9 @@ function Contact() {
         <div className="container-custom">
         <div className="contact">
             <h1 className="heading">Contact Me</h1>
-            <p> 1327 Beacon Ridge Rd apt 1427</p>
-            <p>6072064901</p>
-            <p>matthewdc182@gmail.com</p>
+            <p><i class="fas fa-map-marker-alt"></i> 1327 Beacon Ridge Rd apt 1427</p>
+            <p><i class="fas fa-phone"></i> 6072064901</p>
+            <p><i class="far fa-envelope"></i> matthewdc182@gmail.com</p>
             <p></p>
 
 
