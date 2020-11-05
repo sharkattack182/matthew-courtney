b1f662a33907da9852b29b9acbfd09fd01e47003
Matthew Courtney <matthewdc182@gmail.com>
2020-11-04 22:05:24 -0500

    added in buttons need to remove padding and margins on small screen on both pages with buttons

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -12,7 +12,8 @@ function Contact() {
             <p><i class="fas fa-map-marker-alt"></i> 1327 Beacon Ridge Rd apt 1427</p>
             <p><i class="fas fa-phone"></i> 6072064901</p>
             <p><i class="far fa-envelope"></i> matthewdc182@gmail.com</p>
-            <p></p>
+            <a href="https://www.linkedin.com/in/matthew-courtney-2854811b5/" className="button button-outline button-white">LinkedIn</a>
+            <a href="https://github.com/sharkattack182" className="button button-outline button-white">GitHub</a>
 
 
         </div>
